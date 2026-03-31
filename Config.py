390d9bfa4dd30a5b78b7f011d2d311958df16d10
Zeroz <zerozeezqzero@gmail.com>
2026-03-31 17:24:50 +0300

    pandas

--- Config.py
+++ Config.py
@@ -1,4 +1,1 @@
-API = "5bd6aa01c5ebd6dcf34e557d566c10e8"
-link = f"http://api.weatherstack.com/current?access_key={API}&query="
-data_base_path = "/home/zero/PYTHON/SQL_pogoda/weather.db"
-translations_path = "/home/zero/PYTHON/SQL_pogoda/translations.json"
+*
